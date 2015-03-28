3b5ea1fbd3d15d4faa0050e7387ca29aab83a87e
Sung Won Cho <mikeswcho@gmail.com>
2015-03-28 22:06:19 +1100

    Validate the presence of params[:body]

--- apps/api/controllers/todo/create.rb
+++ apps/api/controllers/todo/create.rb
@@ -2,12 +2,22 @@ module Api::Controllers::Todo
   class Create
     include Api::Action
 
+    before :validate_params
+
     params do
-      param :body
+      param :body, presence: true
     end
 
     def call(params)
       CreateTodo.new(params).call
+
+      self.status = 201
     end
+
+    private
+
+      def validate_params
+        halt 422 unless params.valid?
+      end
   end
 end
--- spec/api/controllers/todo/create_spec.rb
+++ spec/api/controllers/todo/create_spec.rb
@@ -3,16 +3,26 @@ require_relative '../../../../apps/api/controllers/todo/create'
 
 describe Api::Controllers::Todo::Create do
   let(:action) { Api::Controllers::Todo::Create.new }
-  let(:params) { Hash['body', 'Read book'] }
+  let(:valid_params) { Hash['body', 'Read book'] }
+  let(:invalid_params) { Hash['body', ''] }
 
-  it "is successful" do
-    response = action.call(params)
-    expect(response[0]).to eq 200
+  context 'with valid params' do
+    it "returns 201 successful status" do
+      response = action.call(valid_params)
+      expect(response[0]).to eq 201
+    end
+
+    it 'creates a new todo' do
+      expect {
+        action.call(valid_params)
+      }.to change { TodoRepository.all.count }.by(1)
+    end
   end
 
-  it 'creates a new todo' do
-    expect {
-      action.call(params)
-    }.to change { TodoRepository.all.count }.by(1)
+  context 'with invalid params' do
+    it 'returns 422 unprocessible entity status' do
+      response = action.call(invalid_params)
+      expect(response[0]).to eq 422
+    end
   end
 end
